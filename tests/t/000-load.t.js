c10de39b3173fff0746c4c94aaccb3d8dcb0bf38
Stevan Little <stevan.little@iinteractive.com>
2010-12-02 22:27:36 -0500

    okay, compiling the schemas in the repo, time for a beer, we cna test this tomorrow

--- tests/t/000-load.t.js
+++ tests/t/000-load.t.js
@@ -6,14 +6,12 @@ test(
         var jackalope = new Jackalope ();
         ok(jackalope instanceof Jackalope, '... we are an instance of Jackalope');
 
-        var repo = new Jackalope.Schema.Repository ();
+        var repo = new Jackalope.Schema.Repository ({
+            spec_url  : "../spec/spec.json",
+            validator : new Jackalope.Schema.Validator ()
+        });
         ok(repo instanceof Jackalope.Schema.Repository, '... we are an instance of Jackalope.Schema.Repository');
 
-        var validator = new Jackalope.Schema.Validator ();
-        ok(validator instanceof Jackalope.Schema.Validator, '... we are an instance of Jackalope.Schema.Validator');
-
-        var validator_core = new Jackalope.Schema.Validator.Core ();
-        ok(validator_core instanceof Jackalope.Schema.Validator.Core, '... we are an instance of Jackalope.Schema.Validator.Core');
     }
 
 );
