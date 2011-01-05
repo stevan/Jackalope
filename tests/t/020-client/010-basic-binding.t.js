b4efdf1ccafbe0f6eb06f75cb5706251e174a89b
Stevan Little <stevan.little@iinteractive.com>
2011-01-05 14:47:28 -0500

    deferred target handling

--- tests/t/020-client/010-basic-binding.t.js
+++ tests/t/020-client/010-basic-binding.t.js
@@ -72,6 +72,48 @@ test(
             equal(r.get('first_name'), "Scott", "... got the right value for updated resource after changing DOM");
         })();
 
+        (function () {
+            var r = new Jackalope.Client.Resource ({
+                "id"   : "stevan",
+                "body" : {
+                    "first_name" : "Stevan",
+                    "last_name"  : "Little"
+                },
+                "version" : "fe982ce14ce2b2a1c097629adecdeb1522a1e0a2ca390673446c930ca5fd11d2",
+                "links"   : [
+                    { "rel" : "create", "href" : "/",  "method" : "POST"   },
+                    { "rel" : "delete", "href" : "/1", "method" : "DELETE" },
+                    { "rel" : "edit",   "href" : "/1", "method" : "PUT"    },
+                    { "rel" : "list",   "href" : "/",  "method" : "GET"    },
+                    { "rel" : "read",   "href" : "/1", "method" : "GET"    }
+                ]
+            });
+
+            var binding = new Jackalope.Client.Binding ({
+                element  : "<input type='text'/>",
+                property : "first_name"
+            });
+
+            equal(binding.element.val(), "", "... got the right value for the DOM before initial binding");
+
+            binding.set_target( r );
+
+            equal(binding.element.val(), "Stevan", "... got the right value for the DOM after initial binding");
+
+            binding.unregister_target_event();
+            r.set({ first_name : "Steve" });
+
+            equal(binding.element.val(), "Stevan", "... got the right value for the DOM because we unbound the binding");
+
+            binding.register_target_event();
+            binding.refresh();
+
+            equal(binding.element.val(), "Steve", "... got the right value for the DOM because we rebound the binding and refreshed");
+
+            binding.element.val("Scott");
+            binding.element.trigger('change'); // gotta manually trigger this in the test
+            equal(r.get('first_name'), "Scott", "... got the right value for updated resource after changing DOM");
+        })();
 
         (function () {
             var r = new Jackalope.Client.Resource ({
