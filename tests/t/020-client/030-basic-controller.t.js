792c72b71e61b8d6850df7c0907089c5a45921a3
Stevan Little <stevan.little@iinteractive.com>
2011-01-05 15:21:33 -0500

    rethink controllers and bindings

--- tests/t/020-client/030-basic-controller.t.js
+++ tests/t/020-client/030-basic-controller.t.js
@@ -3,31 +3,37 @@ test(
     "Basic Controller test",
     function() {
 
+        var $input = $("<input type='text'/>");
+
+        var resource = new Jackalope.Client.Resource ({
+            "id"   : "stevan",
+            "body" : {
+                "first_name" : "Stevan",
+                "last_name"  : "Little"
+            },
+            "version" : "fe982ce14ce2b2a1c097629adecdeb1522a1e0a2ca390673446c930ca5fd11d2",
+            "links"   : []
+        });
+
         var c = new Jackalope.Client.Controller ({
-            _first_name : "Stevan",
-            first_name  : function ( v ) {
-                if (v == undefined ) return c._first_name;
-                c._first_name = v;
-            }
+            context  : resource,
+            bindings : [
+                new Jackalope.Client.Binding ({
+                    element  : $input,
+                    property : "first_name"
+                })
+            ]
         });
         ok(c instanceof Jackalope.Client.Controller, "... we are an instance of Jackalope.Client.Controller");
 
-        var $input = $("<input type='text'/>");
-
-        var binding = new Jackalope.Client.Binding ({
-            element  : $input,
-            target   : c,
-            property : "first_name"
-        });
-
         equal($input.val(), "Stevan", "... got the right value for the DOM after initial binding");
 
-        c.set("first_name", "Steve");
-        equal(c.get('first_name'), "Steve", "... got the right value for updated resource");
+        c.context.set("first_name", "Steve");
+        equal(c.context.get('first_name'), "Steve", "... got the right value for updated resource");
         equal($input.val(), "Steve", "... got the right value for the DOM after changing resource");
 
         $input.val("Scott");
         $input.trigger('change'); // gotta manually trigger this in the test
-        equal(c.get('first_name'), "Scott", "... got the right value for updated resource after changing DOM");
+        equal(c.context.get('first_name'), "Scott", "... got the right value for updated resource after changing DOM");
     }
 );
