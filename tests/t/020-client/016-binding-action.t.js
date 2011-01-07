3d6cd669ced002df98c3237303e29fd4744becad
Stevan Little <stevan.little@iinteractive.com>
2011-01-07 14:45:21 -0500

    more testss

--- tests/t/020-client/016-binding-action.t.js
+++ tests/t/020-client/016-binding-action.t.js
@@ -4,19 +4,52 @@ test(
     function() {
 
         var object = {
+            // data
+            counter : 0,
+            // methods
             test_one : function () {
                 ok(this === object, '... got the right context');
+            },
+            test_two : function () {
+                this.counter++
             }
         };
 
-        var a = new Jackalope.Client.Binding.Action ({
-            element       : $('<input type="button" />'),
-            event_type    : 'click',
-            target        : object,
-            target_action : 'test_one'
-        });
+        (function () {
+            var a = new Jackalope.Client.Binding.Action ({
+                element       : $('<input type="button" />'),
+                event_type    : 'click',
+                target        : object,
+                target_action : 'test_one'
+            });
 
-        a.element.click();
+            a.element.click();
+        })();
+
+        (function () {
+            var a = new Jackalope.Client.Binding.Action ({
+                element       : $('<input type="button" />'),
+                event_type    : 'click',
+                target_action : 'test_two'
+            });
+
+            equal(object.counter, 0, '... the counter is 0');
+            a.element.click();
+            equal(object.counter, 0, '... the counter is 0');
+
+            a.set_target( object );
+            a.element.click();
+            equal(object.counter, 1, '... the counter is 1');
+
+            a.clear_target();
+            a.element.click();
+            equal(object.counter, 1, '... the counter is 1');
+
+            a.set_target( object );
+            a.element.click();
+            equal(object.counter, 2, '... the counter is 2');
+
+        })();
 
     }
 );
